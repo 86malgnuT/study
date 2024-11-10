55917bf938a709467ea31926ca2fa766783be78c
86malgnuT <86malgnuT.forwork@gmail.com>
2024-11-10 19:29:32 +0700

    [86malgnuT]: Text View and Styles

--- App.tsx
+++ App.tsx
@@ -4,8 +4,26 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.header}>This is header</Text>
+        <Text style={styles.parent}>
+          Parent
+          <Text style={styles.children}>
+            Children
+          </Text>
+        </Text>
+      </View>
+      <Text style={
+        {
+          color: 'red',
+          fontSize: 30,
+          borderColor: 'blue',
+          borderWidth: 1
+        }
+      }>
+        Hello World 1
+      </Text>
+      <Text style={styles.hello}>Hello World 2</Text>
     </View>
   );
 }
@@ -17,4 +35,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello: {
+    color: 'red',
+    fontSize: 30,
+    borderColor: 'blue',
+    borderWidth: 1,
+    padding: 20,
+    marginTop: 20,
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "600",
+  },
+  parent: {
+    fontSize: 60,
+    fontWeight: "600",
+    color: "green"
+  },
+  children: {
+    fontSize: 30,
+    fontWeight: "200",
+  }
 });
