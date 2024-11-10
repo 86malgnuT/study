6f701a65b32d01b7a090f1f462f5e127088f9608
86malgnuT <86malgnuT.forwork@gmail.com>
2024-11-10 19:52:20 +0700

    [86malgnuT]: State and Button

--- App.tsx
+++ App.tsx
@@ -1,29 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState<string>("malgnuT");
+  const [count, setCount] = useState<number>(0);
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.header}>This is header</Text>
-        <Text style={styles.parent}>
-          Parent
-          <Text style={styles.children}>
-            Children
-          </Text>
-        </Text>
-      </View>
-      <Text style={
-        {
-          color: 'red',
-          fontSize: 30,
-          borderColor: 'blue',
-          borderWidth: 1
-        }
-      }>
-        Hello World 1
+      <Text style={{
+        color: "red",
+        fontSize: 30
+      }}>
+        Hello {name}
       </Text>
-      <Text style={styles.hello}>Hello World 2</Text>
+      <View>
+        <Text style={{
+          fontSize: 30
+        }}>
+          Count: {count}
+        </Text>
+        <View style={{}}>
+          <Button title='Increase' onPress={() => setCount(count + 1)} color={"green"} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -35,25 +34,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  hello: {
-    color: 'red',
-    fontSize: 30,
-    borderColor: 'blue',
-    borderWidth: 1,
-    padding: 20,
-    marginTop: 20,
-  },
-  header: {
-    fontSize: 30,
-    fontWeight: "600",
-  },
-  parent: {
-    fontSize: 60,
-    fontWeight: "600",
-    color: "green"
-  },
-  children: {
-    fontSize: 30,
-    fontWeight: "200",
-  }
+
 });
